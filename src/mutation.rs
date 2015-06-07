030489b6ad60d65c7b8a3d009492b8267d4c3f34
Leonard Dahlmann <leo.dahlmann@gmail.com>
2015-06-07 19:18:36 +0200

    implement networks (non-working)

--- src/mutation.rs
+++ src/mutation.rs
@@ -2,28 +2,26 @@ extern crate rand;
 
 use std::f32;
 use std::collections::BTreeSet;
+use std::collections::HashMap;
 use rand::Rng;
 use genes;
-use std::collections::HashMap;
 
 pub type Prob = f64;
 
-pub struct MutationSettings {
-    pub compat_c1: f64,
-    pub compat_c2: f64,
-    pub compat_c3: f64,
-    pub compat_threshold: f64,
+pub struct Settings {
+    pub add_node_prob: Prob,
+    pub add_link_prob: Prob,
 
-    pub add_node_prob: f32,
-    pub add_link_prob: f32,
+    pub link_weight_prob: Prob,
+    pub uniform_perturbation_prob: Prob,
 
-    pub link_weight_prob: f32,
-    pub uniform_perturbation_prob: f32,
+    pub disable_gene_prob: Prob,
 
-    pub disable_gene_prob: f32,
+    pub recurrent_link_prob: Prob,
+    pub self_link_prob: Prob,
 
-    pub no_crossover_prob: f32,
-    pub interspecies_mating_rate: f32,
+    pub no_crossover_prob: Prob,
+    pub interspecies_mating_rate: Prob,
 
     pub keep_champion_min_species_size: i32,
     pub no_stagnant_reproduction_generations: i32,
@@ -38,6 +36,7 @@ pub struct MutationSettings {
 pub struct NewLinkInnovation {
     from_id: genes::NodeId,
     to_id: genes::NodeId,
+    is_recurrent: bool,
 }
 
 /// Stores for each innovation parameter the innovation number that is assigned to the genes
@@ -51,16 +50,12 @@ pub struct NewNodeInnovation {
     old_innovation: usize, 
 }
 
-/// Stores for each innovation parameter the innovation numbers of the two new links
-pub type NewNodeInnovations = HashMap<NewNodeInnovation, (usize, usize)>;
+/// Stores for each innovation parameter the node id of the created node
+/// and the innovation numbers of the two new links
+pub type NewNodeInnovations = HashMap<NewNodeInnovation, (genes::NodeId, usize, usize)>;
 
-pub static STANDARD_MUTATION_SETTINGS : MutationSettings =
-    MutationSettings {
-        compat_c1: 1.0,
-        compat_c2: 1.0,
-        compat_c3: 0.4,
-        compat_threshold: 3.0,
-        
+pub static STANDARD_SETTINGS: Settings =
+    Settings {
         add_node_prob: 0.03,
         add_link_prob: 0.05,
 
@@ -69,6 +64,9 @@ pub static STANDARD_MUTATION_SETTINGS : MutationSettings =
 
         disable_gene_prob: 0.75,
 
+        recurrent_link_prob: 0.3,
+        self_link_prob: 0.5,
+
         no_crossover_prob: 0.25,
         interspecies_mating_rate: 0.001,
 
@@ -76,7 +74,7 @@ pub static STANDARD_MUTATION_SETTINGS : MutationSettings =
         no_stagnant_reproduction_generations: 15
     };                   
 
-pub fn mutate(settings: &MutationSettings, genome: &mut genes::Genome) {
+pub fn mutate(settings: &Settings, genome: &mut genes::Genome) {
     let num_mutations = (genome.num_nodes() as f32).sqrt().floor();
 }
 
@@ -89,18 +87,17 @@ pub fn rand_pos_neg<R: rand::Rng>(rng: &mut R) -> f64 {
 
 /// Add a new node to the genome by inserting it in the middle of an existing link between two nodes.
 /// This function takes a set of node innovations that happened in this generation so far as a parameter.
-pub fn mutation_add_node<R: rand::Rng>(rng: &mut R,
-                                       innovations: &mut NewNodeInnovations,
-                                       innovation_counter: &mut usize,
-                                       node_counter: &mut usize,
-                                       genome: &mut genes::Genome) {
+pub fn new_node<R: rand::Rng>(rng: &mut R,
+                              innovations: &mut NewNodeInnovations,
+                              innovation_counter: &mut usize,
+                              node_counter: &mut usize,
+                              genome: &mut genes::Genome) {
     // Select a link gene to split up. The link must not be in a disabled state. 
     let enabled_gene_indices = 
         genome.links.iter().enumerate()
               .filter(|&(i, link)| link.enabled)
               .map(|(i, link)| i)
               .collect::<Vec<usize>>();
-        //genome.links.iter_mut().filter(|link| link.enabled).collect::<Vec<&mut genes::Link>>();
     
     match rng.choose(&enabled_gene_indices) {
         Some(index) => {
@@ -115,30 +112,37 @@ pub fn mutation_add_node<R: rand::Rng>(rng: &mut R,
                                                          to_id: link.to_id,
                                                          old_innovation: link.innovation };
 
-                let (innovation1, innovation2) = match innovations.get(&new_node_innov) {
-                    Some(numbers) => *numbers, 
+                let (is_new, (new_node_id, innovation1, innovation2)) = match innovations.get(&new_node_innov) {
+                    Some(numbers) => (false, *numbers), 
                     None => {
                         // We have a new innovation
+                        *node_counter += 1;
                         *innovation_counter += 2;
-                        (*innovation_counter-2, *innovation_counter-1)
+
+                        let numbers = (*node_counter-1, *innovation_counter-2, *innovation_counter-1);
+                        (true, numbers)
                     }
                 };
 
-                let new_node_id = *node_counter;
-                *node_counter += 1;
+                if is_new {
+                    innovations.insert(new_node_innov, (new_node_id, innovation1, innovation2));
+                }
 
                 // Now we can create the new genes
                 let link1 = genes::Link { from_id: link.from_id, 
                                           to_id: new_node_id,
                                           enabled: true,
                                           innovation: innovation1,
-                                          weight: 1.0 };
+                                          weight: 1.0,
+                                          is_recurrent: link.is_recurrent };
                 let link2 = genes::Link { from_id: new_node_id,
                                           to_id: link.to_id,
                                           enabled: true,
                                           innovation: innovation2,
-                                          weight: link.weight };
+                                          weight: link.weight,
+                                          is_recurrent: false }; // ???
                 let node = genes::Node { id: new_node_id,
+                                         node_type: genes::NodeType::Hidden,
                                          bias: 0.0 };
 
                 (link1, link2, node)
@@ -151,6 +155,88 @@ pub fn mutation_add_node<R: rand::Rng>(rng: &mut R,
 
         None => ()
     }
+}
+
+/// Add a new link between two nodes. The two nodes are selected at random.
+/// We make `num_tries` tries to find two compatible nodes.
+pub fn new_link<R: rand::Rng>(rng: &mut R,
+                              innovations: &mut NewLinkInnovations,
+                              innovation_counter: &mut usize,
+                              recurrent_link_prob: Prob,
+                              self_link_prob: Prob,
+                              num_tries: usize,
+                              genome: &mut genes::Genome) {
+    let node_indices = (0..genome.nodes.len()-1).collect::<Vec<usize>>();
+    let hidden_node_indices = 
+        genome.nodes.iter().enumerate()
+              .filter(|&(i, node)| node.node_type == genes::NodeType::Hidden)
+              .map(|(i, link)| i).collect::<Vec<usize>>();
+
+    if node_indices.is_empty() || hidden_node_indices.is_empty() {
+        return;
+    }
+
+    // Decide whether to create a recurrent or a feed-forward link
+    let recurrent = rng.next_f64() < recurrent_link_prob;
+
+    // Randomly select from and to node until they fit our criterion
+    let mut from_index = 0;
+    let mut to_index = 0;
+    let mut found = false;
+
+    for try in 0..num_tries {
+        if recurrent && rng.next_f64() < self_link_prob {
+            // Sometimes make a self loop
+            from_index = *rng.choose(&hidden_node_indices).unwrap();
+            to_index = from_index 
+        } else {
+            from_index = *rng.choose(&node_indices).unwrap();
+            to_index = *rng.choose(&hidden_node_indices).unwrap();
+        }
+
+        if !genome.is_link(from_index, to_index) &&
+           genome.is_new_link_recurrent(from_index, to_index) == recurrent {
+            found = true;
+            break;
+        }
+    }
+
+    if !found {
+        return
+    }
+
+    let from_node = &genome.nodes[from_index];
+    let to_node = &genome.nodes[to_index];
+
+    // See if this innovation has already happened in this generation.
+    // If yes, we will use the same innovation number for the new link gene.
+    let new_link_innov = NewLinkInnovation { from_id: from_node.id,
+                                             to_id: to_node.id,
+                                             is_recurrent: recurrent };
+
+    let (is_new, innovation) = match innovations.get(&new_link_innov) {
+        Some(innovation) => (false, *innovation), 
+        None => {
+            // We have a new innovation
+            *innovation_counter += 1;
+            (true, *innovation_counter-1)
+        }
+    };
+
+    if is_new {
+        innovations.insert(new_link_innov, innovation);
+    }
+
+    // Create the new link gene    
+    let weight = rand_pos_neg(rng) * rng.next_f64();
+    let link = genes::Link { from_id: from_node.id, 
+                             to_id: to_node.id,
+                             enabled: true,
+                             innovation: innovation,
+                             weight: weight,
+                             is_recurrent: recurrent };
+
+    genome.links.push(link);
 }
 
 pub enum LinkMutation {
@@ -176,7 +262,7 @@ pub fn rand_link_mutation<R: rand::Rng>(rng: &mut R, perturbate_point: f64, rese
 /// * Perturb adds a random value in `(-power,power)` to the link weight.
 /// * Reset sets the link weight to a random value in `(-power,power)`.
 /// * None leaves the link weight unmodified.
-pub fn mutate_link_weights<R: rand::Rng, F: FnMut(&mut R, usize) -> LinkMutation>(rng: &mut R, mut f: F, power: f64, genome: &mut genes::Genome) {
+pub fn change_link_weights<R: rand::Rng, F: FnMut(&mut R, usize) -> LinkMutation>(rng: &mut R, mut f: F, power: f64, genome: &mut genes::Genome) {
     for (position, ref mut link) in genome.links.iter_mut().enumerate() {
         match f(rng, position) {
             LinkMutation::Perturbate =>
@@ -188,7 +274,7 @@ pub fn mutate_link_weights<R: rand::Rng, F: FnMut(&mut R, usize) -> LinkMutation
     }
 }
 
-pub fn mutate_link_weights_standard<R: rand::Rng>(rng: &mut R, rate: f64, power: f64, genome: &mut genes::Genome) {
+pub fn change_link_weights_standard<R: rand::Rng>(rng: &mut R, rate: f64, power: f64, genome: &mut genes::Genome) {
     let severe = rng.gen::<bool>();
     let num_links = genome.links.len();
 
@@ -212,9 +298,9 @@ pub fn mutate_link_weights_standard<R: rand::Rng>(rng: &mut R, rate: f64, power:
         }
     };
 
-    mutate_link_weights(rng, f, power, genome);
+    change_link_weights(rng, f, power, genome);
 }
 
-pub fn mutate_link_weights_reset_all<R: rand::Rng, F: Fn(usize) -> LinkMutation>(rng: &mut R, power: f64, genome: &mut genes::Genome) {
-    mutate_link_weights(rng, |_, _| LinkMutation::Reset, power, genome);
+pub fn change_link_weights_reset_all<R: rand::Rng, F: Fn(usize) -> LinkMutation>(rng: &mut R, power: f64, genome: &mut genes::Genome) {
+    change_link_weights(rng, |_, _| LinkMutation::Reset, power, genome);
 }
